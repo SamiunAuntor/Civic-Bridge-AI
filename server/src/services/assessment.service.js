87dfc795b432aaa6163e992c3270513613bdf89e
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2026-06-17 20:23:43 +0600

    updated the assessment services

--- server/src/services/assessment.service.js
+++ server/src/services/assessment.service.js
@@ -2,8 +2,10 @@ const assessmentRepository = require("../repositories/assessment.repository");
 
 const { analyzeSituation, } = require("./ai/situation-analysis.service");
 
+const { generatePriorities, } = require("./ai/priority-engine.service");
+
 const createAssessment = async (userId, situation) => {
-    
+
     const analysis = await analyzeSituation(situation);
 
     const { data } =
