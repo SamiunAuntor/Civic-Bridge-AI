1b9080f6093da5064d761b63d5a253f2fe9f126d
Khandaker Ali Ariyan <khandakeraliariyanndc@gmail.com>
2026-06-17 19:31:03 +0600

    connected the the user routes

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 
-const testRoutes = require("./routes/test.routes");
+const userRoutes = require("./routes/user.routes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/test", testRoutes);
+app.use("/api/users", userRoutes);
 
 module.exports = app;
